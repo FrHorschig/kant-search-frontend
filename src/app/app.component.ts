727ce38eaff3a243b33b7f15b6fca08714f28e24
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-22 18:31:06 +0200

    feat: move navigation bar to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(private readonly translateService: TranslateService) {
+  items: MenuItem[] = [
+    {
+      label: 'NAVBAR.READ',
+      icon: 'pi pi-eye',
+      command: () => this.router.navigate(['/read']),
+    },
+    {
+      label: 'NAVBAR.SEARCH',
+      icon: 'pi pi-search',
+      command: () => this.router.navigate(['/search']),
+    },
+    {
+      label: 'NAVBAR.ADMIN',
+      icon: 'pi pi-shield',
+      command: () => this.router.navigate(['/admin']),
+    },
+  ];
+
+  constructor(
+    private readonly router: Router,
+    private readonly translateService: TranslateService
+  ) {
     this.translateService.setDefaultLang('en');
   }
 }
--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -1,29 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-startpage',
   templateUrl: './startpage.component.html',
 })
-export class StartpageComponent {
-  items: MenuItem[] = [
-    {
-      label: 'NAVBAR.READ',
-      icon: 'pi pi-eye',
-      command: () => this.router.navigate(['/read']),
-    },
-    {
-      label: 'NAVBAR.SEARCH',
-      icon: 'pi pi-search',
-      command: () => this.router.navigate(['/search']),
-    },
-    {
-      label: 'NAVBAR.ADMIN',
-      icon: 'pi pi-shield',
-      command: () => this.router.navigate(['/admin']),
-    },
-  ];
-
-  constructor(private readonly router: Router) {}
-}
+export class StartpageComponent {}
