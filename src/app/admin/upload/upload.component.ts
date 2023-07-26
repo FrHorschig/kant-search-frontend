40d1e5c2175895f8705a18c86bcd602a9b7a2f26
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-26 15:59:46 +0200

    refactor: update service names from api package

--- src/app/admin/upload/upload.component.ts
+++ src/app/admin/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpError, Work, WorksService } from 'kant-search-api';
+import { HttpError, UploadService, Work } from 'kant-search-api';
 import { MessageService } from 'primeng/api';
 import { SmartComponent } from 'src/app/common/base/smart.component';
 
@@ -17,7 +17,7 @@ export class UploadComponent extends SmartComponent {
 
   constructor(
     private readonly messageService: MessageService,
-    private readonly worksService: WorksService
+    private readonly uploadService: UploadService
   ) {
     super();
   }
@@ -49,8 +49,8 @@ export class UploadComponent extends SmartComponent {
 
   postText(work: Work) {
     this.messageService.clear();
-    this.worksService
-      .postWork(work)
+    this.uploadService
+      .uploadWork(work)
       .pipe(this.takeUntilDestroy())
       .subscribe({
         next: () => {
