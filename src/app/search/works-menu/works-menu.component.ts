cb983f4797fc6b622d08fff57e7928e758bc2f88
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-28 17:34:45 +0200

    feat: add checkbox to select/unselect all nodes

--- src/app/search/works-menu/works-menu.component.ts
+++ src/app/search/works-menu/works-menu.component.ts
@@ -18,7 +18,13 @@ export class WorksMenuComponent {
     if (!works) {
       return;
     }
-    this.nodes = this.treeBuilder.buildTree(works);
+    this.nodes = [];
+    this.nodes.push({
+      label: `All`, // TODO: i18n
+      selectable: true,
+      expanded: true,
+      children: this.treeBuilder.buildTree(works),
+    });
   }
 
   constructor(private readonly treeBuilder: WorkTreeBuilderService) {}
