a8f22dd5ccbbc85afb4c6392e31a56ef9d0e53a3
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-29 08:39:47 +0200

    fix: clear messages when navigating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -12,23 +12,33 @@ export class AppComponent {
     {
       label: 'NAVBAR.READ',
       icon: 'pi pi-eye',
-      command: () => this.router.navigate(['/read']),
+      command: () => {
+        this.messageService.clear();
+        this.router.navigate(['/read']);
+      },
     },
     {
       label: 'NAVBAR.SEARCH',
       icon: 'pi pi-search',
-      command: () => this.router.navigate(['/search']),
+      command: () => {
+        this.messageService.clear();
+        this.router.navigate(['/search']);
+      },
     },
     {
       label: 'NAVBAR.ADMIN',
       icon: 'pi pi-shield',
-      command: () => this.router.navigate(['/admin']),
+      command: () => {
+        this.messageService.clear();
+        this.router.navigate(['/admin']);
+      },
     },
   ];
 
   constructor(
     private readonly router: Router,
-    private readonly translateService: TranslateService
+    private readonly translateService: TranslateService,
+    private readonly messageService: MessageService
   ) {
     this.translateService.setDefaultLang('en');
   }
