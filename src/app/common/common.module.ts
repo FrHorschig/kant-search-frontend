24dfc9a9a94b1a85ef53e4ecdfaa83f1693f7b08
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-07-29 13:24:54 +0200

    refactor: highlight search terms in backend

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -3,20 +3,11 @@ import { CommonModule as NgCommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { TreeModule } from 'primeng/tree';
 import { PagenumberNormalModePipe } from './pipes/pagenumber-normal-mode/pagenumber-normal-mode.pipe';
-import { HighlightSearchTermsPipe } from './pipes/highlight-search-terms/highlight-search-terms.pipe';
 import { WorksMenuComponent } from './shared/works-menu/works-menu.component';
 
 @NgModule({
-  declarations: [
-    WorksMenuComponent,
-    PagenumberNormalModePipe,
-    HighlightSearchTermsPipe,
-  ],
+  declarations: [WorksMenuComponent, PagenumberNormalModePipe],
   imports: [NgCommonModule, TranslateModule, TreeModule],
-  exports: [
-    WorksMenuComponent,
-    PagenumberNormalModePipe,
-    HighlightSearchTermsPipe,
-  ],
+  exports: [WorksMenuComponent, PagenumberNormalModePipe],
 })
 export class CommonModule {}
--- src/app/common/pipes/highlight-search-terms/highlight-search-terms.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightSearchTermsPipe } from './highlight-search-terms.pipe';
-
-describe('HighlightSearchTermsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HighlightSearchTermsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
