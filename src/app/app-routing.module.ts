c07f9dac1ab809f4ac00f57e699e0a80dc223fbc
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-18 21:02:31 +0200

    feat: add canLoad guard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartpageComponent } from './startpage/startpage.component';
+import { worksGuard } from './store/works/works-guard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/startpage', pathMatch: 'full' },
   { path: 'startpage', component: StartpageComponent },
   {
     path: 'read',
+    canLoad: [worksGuard],
     loadChildren: () => import('./read/read.module').then((m) => m.ReadModule),
   },
   {
     path: 'search',
+    canLoad: [worksGuard],
     loadChildren: () =>
       import('./search/search.module').then((m) => m.SearchModule),
   },
   {
     path: 'admin',
+    canLoad: [worksGuard],
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
