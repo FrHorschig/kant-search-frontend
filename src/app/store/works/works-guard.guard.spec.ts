3ed2aed6b3bb34f5d3286f856f7facf763d32458
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-20 14:27:39 +0200

    test: add tests for WorksLoadedService

--- src/app/store/works/works-guard.guard.spec.ts
+++ src/app/store/works/works-guard.guard.spec.ts
@@ -1,18 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { WorksLoadedService } from './works-guard.guard';
-
-// TODO frhorsch: add tests for works-guard
+import { WorksReducers } from '.';
 
 describe('WorksLoadedService', () => {
   let service: WorksLoadedService;
+  let store: MockStore;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [WorksLoadedService, provideMockStore()],
+    });
     service = TestBed.inject(WorksLoadedService);
+    store = TestBed.inject(MockStore);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should update isLoaded when store emits value', () => {
+    const mockIsLoaded = true;
+    store.overrideSelector(WorksReducers.selectIsLoaded, mockIsLoaded);
+
+    store.refreshState();
+
+    expect(service.isLoaded).toBe(mockIsLoaded);
+  });
+
+  it('should unsubscribe from the store on destroy', () => {
+    spyOn(service['subscription'], 'unsubscribe');
+
+    service.ngOnDestroy();
+
+    expect(service['subscription'].unsubscribe).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    service.ngOnDestroy(); // Clean up any subscriptions
   });
 });
