365ae42ae2c4b8dbc48fbf223ec2977f09f8ad38
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-21 22:39:05 +0200

    test: add tests for normal mode pipe

--- src/app/common/pipes/pagenumber-normal-mode.pipe.spec.ts
+++ src/app/common/pipes/pagenumber-normal-mode.pipe.spec.ts
@@ -1,8 +1,56 @@
 import { PagenumberNormalModePipe } from './pagenumber-normal-mode.pipe';
+import { TestBed } from '@angular/core/testing';
 
 describe('PagenumberNormalModePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PagenumberNormalModePipe();
+  let pipe: PagenumberNormalModePipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PagenumberNormalModePipe],
+    });
+    pipe = new PagenumberNormalModePipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should replace {p#} with the appropriate span', () => {
+    const result = pipe.transform('This is a test {p123} string');
+    expect(result).toBe(
+      'This is a test <span class="ks-pagenr-normal">[123]</span> string'
+    );
+  });
+
+  it('should replace {p#} followed by <span class="ks-h"> with a break and appropriate spans', () => {
+    const result = pipe.transform(
+      'Another test {p456} <span class="ks-h"> example'
+    );
+    expect(result).toBe(
+      'Another test <span class="ks-pagenr-normal">[456]</span> </br><span class="ks-h"> example'
+    );
+  });
+
+  it('should handle multiple replacements with line break', () => {
+    const result = pipe.transform(
+      'Multiple {p789} instances {p101} <span class="ks-h"> in a sentence'
+    );
+    expect(result).toBe(
+      'Multiple <span class="ks-pagenr-normal">[789]</span> instances <span class="ks-pagenr-normal">[101]</span> </br><span class="ks-h"> in a sentence'
+    );
+  });
+
+  it('should handle multiple replacements without line break', () => {
+    const result = pipe.transform(
+      'Multiple {p789} instances {p101} in a <span class="ks-h"> sentence'
+    );
+    expect(result).toBe(
+      'Multiple <span class="ks-pagenr-normal">[789]</span> instances <span class="ks-pagenr-normal">[101]</span> in a <span class="ks-h"> sentence'
+    );
+  });
+
+  it('should return the original string if there are no matches', () => {
+    const result = pipe.transform('A simple string without patterns.');
+    expect(result).toBe('A simple string without patterns.');
   });
 });
--- src/app/common/pipes/pagenumber-normal-mode.pipe.ts
+++ src/app/common/pipes/pagenumber-normal-mode.pipe.ts
@@ -6,9 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PagenumberNormalModePipe implements PipeTransform {
   transform(text: string): string {
     return text.replace(
+      // replace {p#} with [#] where '#' is a number
+      // if it's followed by a 'ks-h' span, add a line break
       /\s*\{p(\d+)\}\s*(<span class="ks-h">)?/g,
-      (match, p1, p2) => {
-        return ` <span class="ks-page-no-normal">[${p1}]</span> ${
+      (_, p1, p2) => {
+        return ` <span class="ks-pagenr-normal">[${p1}]</span> ${
           p2 ? '</br>' + p2 : ''
         }`;
       }
