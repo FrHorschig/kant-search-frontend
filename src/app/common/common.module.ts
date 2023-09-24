0f51d19c32c2f8f40af0ecc73cafb48a07f81587
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-24 09:53:30 +0200

    feat: add labels and info tooltips

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -4,10 +4,16 @@ import { TranslateModule } from '@ngx-translate/core';
 import { TreeModule } from 'primeng/tree';
 import { PagenumberNormalModePipe } from './pipes/pagenumber-normal-mode.pipe';
 import { WorksMenuComponent } from './shared/works-menu/works-menu.component';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputGroupComponent } from './shared/input-group/input-group.component';
 
 @NgModule({
-  declarations: [WorksMenuComponent, PagenumberNormalModePipe],
-  imports: [NgCommonModule, TranslateModule, TreeModule],
-  exports: [WorksMenuComponent, PagenumberNormalModePipe],
+  declarations: [
+    WorksMenuComponent,
+    PagenumberNormalModePipe,
+    InputGroupComponent,
+  ],
+  imports: [NgCommonModule, TranslateModule, TreeModule, TooltipModule],
+  exports: [WorksMenuComponent, PagenumberNormalModePipe, InputGroupComponent],
 })
 export class CommonModule {}
---
+++ src/app/common/shared/input-group/input-group.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-group',
+  templateUrl: './input-group.component.html',
+})
+export class InputGroupComponent {
+  @Input() idString = '';
+  @Input() i18nString = '';
+}
