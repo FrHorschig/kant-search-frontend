bbd5ce8d9b475782e30accf6ecea15ae278e2052
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-09-25 11:47:36 +0200

    refactor: rename style class for pagination

--- src/app/common/pipes/normal-mode.pipe.ts
+++ src/app/common/pipes/normal-mode.pipe.ts
@@ -10,7 +10,7 @@ export class NormalModePipe implements PipeTransform {
       // if it's followed by a 'ks-h' span, add a line break
       /\s*\{p(\d+)\}\s*(<span class="ks-h">)?/g,
       (_, p1, p2) => {
-        return ` <span class="ks-pagenr-normal">[${p1}]</span> ${
+        return ` <span class="ks-pagination-s">[${p1}]</span> ${
           p2 ? '</br>' + p2 : ''
         }`;
       }
