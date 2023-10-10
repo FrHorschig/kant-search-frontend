f8b0e1ba3020dd973a4fb9bb8e524e3db8886452
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-10-10 23:02:21 +0200

    refactor: improve original mode

--- src/app/read/pipes/original-mode.pipe.ts
+++ src/app/read/pipes/original-mode.pipe.ts
@@ -10,30 +10,38 @@ export class OriginalModePipe implements PipeTransform {
         // replace {p#} with [#] where '#' is a number and add a line break
         /\s*\{p(\d+)\}\s*(<span class="ks-h">)?/g,
         (_, p1, p2) => {
-          return `${
-            p2 ? '' : '</br>'
-          }</br><span class="ks-pagination-m">[${p1}]</span>${p2 ? p2 : ''}`;
+          return `${p2 ? '' : '</br>'}</br>
+          <span class="ks-pagination-m">[${p1}]</span>
+          ${p2 ? p2 : ''}`;
         }
       )
       .replace(
         /\s*\{l(\d+)\}\s*/g, // replace {l#} with [#] where '#' is a number
         (_, p) => {
-          return `</br>${this.s3()}<span class="ks-pagination-s">${this.addLeadingSpace(
-            p
-          )}</span>${this.s3()}`;
+          const num = parseInt(p, 10);
+          return num % 5 == 0
+            ? `</br>${this.s3()}<span class="ks-pagination-s">
+              ${this.addLeadingSpace(num)}
+              </span>${this.s3()}`
+            : `</br>${this.s3()}<span class="ks-pagination-s">
+              ${this.s3()}.
+              </span>${this.s3()}`;
         }
       );
   }
 
-  private addLeadingSpace(n: string) {
-    const num = parseInt(n, 10);
-    if (num < 10) {
-      return `0${num}`;
+  private addLeadingSpace(n: number) {
+    if (n < 10) {
+      return `0${n}`;
     }
-    return num.toString();
+    return n.toString();
   }
 
   private s3(): string {
     return `&nbsp;&nbsp;&nbsp;`;
+  }
+
+  private s9(): string {
+    return `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
   }
 }
