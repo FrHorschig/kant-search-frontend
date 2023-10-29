3cd3a7fd3d06a5fcc793cd75d16356be6bb3ecdd
Friedrich Horschig <friedrich.horschig@mailbox.org>
2023-10-29 15:53:25 +0100

    refactor: simplify regexes and add tests

--- src/app/search/pipes/remove-pagination.pipe.spec.ts
+++ src/app/search/pipes/remove-pagination.pipe.spec.ts
@@ -48,8 +48,53 @@ describe('NormalModePipe', () => {
     expect(result).toBe(' some words');
   });
 
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('234.5} some words');
+    expect(result).toBe(' some words');
+  });
+
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('5} some words');
+    expect(result).toBe(' some words');
+  });
+
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('} some words');
+    expect(result).toBe(' some words');
+  });
+
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('fr234.5} some words');
+    expect(result).toBe(' some words');
+  });
+
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('fr234.5} some words');
+    expect(result).toBe(' some words');
+  });
+
+  it('should remove pagination cut off at start', () => {
+    const result = pipe.transform('fr234.5} some words');
+    expect(result).toBe(' some words');
+  });
+
   it('should remove pagination cut off at end', () => {
     const result = pipe.transform('some words {fr234.5');
+    expect(result).toBe('some words ');
+  });
+
+  it('should remove pagination cut off at end 2', () => {
+    const result = pipe.transform('some words {fn234');
+    expect(result).toBe('some words ');
+  });
+
+  it('should remove pagination cut off at end 3', () => {
+    const result = pipe.transform('some words {p');
+    expect(result).toBe('some words ');
+  });
+
+  it('should remove pagination cut off at end 4', () => {
+    const result = pipe.transform('some words {');
     expect(result).toBe('some words ');
   });
 });
--- src/app/search/pipes/remove-pagination.pipe.ts
+++ src/app/search/pipes/remove-pagination.pipe.ts
@@ -10,20 +10,11 @@ export class RemovePaginationPipe implements PipeTransform {
     }
     return (
       text
-        .replace(/\s*\{p\d{1,5}\}\s*/g, ' ')
-        .replace(/\s*\{l\d{1,3}\}\s*/g, ' ')
-        .replace(/\s*\{fr\d{1,5}\.\d{1,3}\}\s*/g, ' ')
-        .replace(/\s*\{fn\d{1,5}\.\d{1,3}\}\s*/g, ' ')
+        .replace(/\s*\{(p|l|fr|fn)\d+(?:\.\d+)?\}\s*/g, ' ')
         // handle cases where the end of the pagination string is cut off
-        .replace(/\s*\{p\d{0,5} /g, ' ')
-        .replace(/\s*\{l\d{0,5} /g, ' ')
-        .replace(/\s*\{fr\d{0,5}(?:\.\d{0,3})?/g, ' ')
-        .replace(/\s*\{fn\d{0,5}(?:\.\d{0,3})?/g, ' ')
+        .replace(/\s*\{(p|l|fr|fn)?(\d*)?(?:\.\d*)?/g, ' ')
         // handle cases where the start of the pagination string is cut off
-        .replace(/p\d{1,5}\}\s*/g, ' ')
-        .replace(/l\d{1,5}\}\s*/g, ' ')
-        .replace(/fr\d{1,5}\.\d{1,3}\}\s*/g, ' ')
-        .replace(/fn\d{1,5}\.\d{1,3}\}\s*/g, ' ')
+        .replace(/(p|l|fr|fn)?(\d+)?(?:\.\d+)?\}\s*/g, ' ')
     );
   }
 }
