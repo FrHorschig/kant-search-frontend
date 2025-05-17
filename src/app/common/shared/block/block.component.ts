c9f11ae589fa94712c92035ae1c16d42b5ff7173
Friedrich Horschig <frhorschig-coding@mailbox.org>
2025-05-17 20:57:52 +0200

    convert block text to safe html

--- src/app/common/shared/block/block.component.ts
+++ src/app/common/shared/block/block.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
-    selector: 'ks-text-block',
-    templateUrl: './block.component.html',
-    standalone: false
+  selector: 'ks-text-block',
+  templateUrl: './block.component.html',
+  standalone: false,
 })
 export class TextBlockComponent {
   @Input() text: string = '';
+  private sanitizer = inject(DomSanitizer);
+
+  trustText(text: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(text);
+  }
 }
