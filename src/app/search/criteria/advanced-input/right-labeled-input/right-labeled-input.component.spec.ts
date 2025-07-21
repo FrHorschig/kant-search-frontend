190126947dcfce6570380fbb9db6e23517e0464f
Friedrich Horschig <frhorschig-coding@mailbox.org>
2025-07-21 16:54:07 +0200

    simplify advanced options

--- src/app/search/criteria/advanced-input/right-labeled-input/right-labeled-input.component.spec.ts
+++ src/app/search/criteria/advanced-input/right-labeled-input/right-labeled-input.component.spec.ts
@@ -11,6 +11,7 @@ import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { RightLabeledInputComponent } from './right-labeled-input.component';
+import { CommonModule } from '@angular/common';
 
 describe('RightLabeledInputComponent', () => {
   let component: RightLabeledInputComponent;
@@ -29,6 +30,7 @@ describe('RightLabeledInputComponent', () => {
       .overrideComponent(RightLabeledInputComponent, {
         set: {
           imports: [
+    CommonModule,
             TranslateModule,
             NzFlexModule,
             NzGridModule,
