7018129a8bac3d755633d366b69954696b5d1034
Friedrich Horschig <frhorschig-coding@mailbox.org>
2025-09-27 12:52:28 +0200

    add url-copy button to search result page

--- src/app/search/results/results-input/results-input.component.ts
+++ src/app/search/results/results-input/results-input.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
@@ -14,7 +15,7 @@ import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
   imports: [
     FormsModule,
     TranslateModule,
-    NzSpaceModule,
+    NzGridModule,
     NzButtonModule,
     NzToolTipModule,
     NzIconModule,
@@ -27,11 +28,23 @@ export class ResultsInputComponent {
   @Output() searchTermsEmitter = new EventEmitter<string>();
   @Output() doUpdateEmitter = new EventEmitter<void>();
 
+  isCopied = false;
+
   onSearchTermsChange(searchTerms: string) {
     this.searchTermsEmitter.emit(searchTerms);
   }
 
   onSubmit() {
     this.doUpdateEmitter.emit();
+  }
+
+  onCopyClick(): void {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url).then(() => {
+      this.isCopied = true;
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 1500);
+    });
   }
 }
