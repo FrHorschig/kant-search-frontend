55327a3a8f77df3ba1cd7ab2eda85be070e8846d
Friedrich Horschig <frhorschig-coding@mailbox.org>
2026-09-19 16:35:34 +0200

    simplify base path replacement

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sed -i "s|<base href=\"/\" />|<base href=\"/${BASE_PATH}\"|g" \
+sed -i "s|base href=\"/\"|base href=\"${BASE_PATH}\"|" \
     /usr/share/nginx/html/index.html
 
 exec nginx -g 'daemon off;'
