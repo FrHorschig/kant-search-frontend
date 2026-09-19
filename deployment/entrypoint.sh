9b282c6150882f640d8fcded694d83244698ae16
Friedrich Horschig <frhorschig-coding@mailbox.org>
2026-09-19 15:47:25 +0200

    update base path and its replacement script

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sed -i "s|__BASE_PATH__|${BASE_PATH}|g" \
+sed -i "s|<base href=\"/\" />|<base href=\"/${BASE_PATH}\"|g" \
     /usr/share/nginx/html/index.html
 
 exec nginx -g 'daemon off;'
