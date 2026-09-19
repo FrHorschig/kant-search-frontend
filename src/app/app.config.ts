4771c0672616d745c2a7495e642fe73feaaebb06
Friedrich Horschig <frhorschig-coding@mailbox.org>
2026-09-19 15:48:04 +0200

    use relative path for i18n files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -26,10 +26,11 @@ export const appProviders = [
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+        useFactory: (http: HttpClient) =>
+          new TranslateHttpLoader(http, './assets/i18n/'),
         deps: [HttpClient],
       },
-    })
+    }),
   ),
   provideAppInitializer(() => {
     const configStore = inject(ConfigStore);
